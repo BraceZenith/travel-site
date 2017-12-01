914d460ee83ae09112cd0ba512e8caf6307bc62a
Brace <bracezenith@gmail.com>
2017-12-01 16:50:14 +0100

    #39(14.1) Started JS organization, structure and object-oriented programming.

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,77 @@
+// Goal
+
+// console.log("Hello, my name is John Doe and my favorite color is blue.");
+// console.log("Hello, my name is Jane Smith and my favorite color is green.");
+
+
+
+// First iteration
+
+// function person(name, favColor) {
+// 	console.log("Hello, my name is " + name + " and my favorite color is " + favColor + ".");
+// }
+
+// person("John Doe", "blue");
+// person("Jane Smith", "green");
+
+
+
+// Second iteration
+
+// function person(name, favColor) {
+// 	console.log("Hello, my name is " + name + " and my favorite color is " + favColor + ".");
+// }
+
+// var johnName = "John Doe";
+// var johnFavColor = "blue";
+
+// person(johnName, johnFavColor);
+// person("Jane Smith", "green");
+
+
+
+// Third iteration
+
+// function person(name, favColor) {
+// 	console.log("Hello, my name is " + name + " and my favorite color is " + favColor + ".");
+// }
+
+// var john = {
+// 	name: "John Doe",
+// 	favoriteColor: "blue"
+// }
+
+// person(john.name, john.favoriteColor);
+// person("Jane Smith", "green");
+
+
+
+// Fourth iteration
+
+// var john = {
+// 	name: "John Doe",
+// 	favoriteColor: "blue",
+// 	greet: function() {
+// 		console.log("Hello, my name is " + john.name + " and my favorite color is " + john.favoriteColor + ".");
+// 	}
+// }
+
+// john.greet();
+
+
+
+// Fifth iteration
+
+function Person(fullName, favColor) {
+	this.name = fullName;
+	this.favoriteColor = favColor;
+	this.greet = function() {
+		console.log("Hello, my name is " + this.name + " and my favorite color is " + this.favoriteColor + ".");
+	}
+}
+
+var john = new Person("John Doe", "blue");
+john.greet();
+
+var jane = new Person("Jane Smith", "green");
+jane.greet();
