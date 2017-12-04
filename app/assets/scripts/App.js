de00ce3362476a48ef1d6024ecf37ea990d2eaca
Brace <bracezenith@gmail.com>
2017-12-04 16:59:46 +0100

    #43-#45(section 15) Completed desktop and mobile header styles and functionality.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,23 +1,3 @@
-var $ = require('jquery');
-// Node.js way
-// var Person = require('./modules/Person');
+import MobileMenu from './modules/MobileMenu';
 
-// ES6 way
-import Person from './modules/Person';
-
-class Adult extends Person {
-	payTaxes() {
-		console.log(this.name + " now owes $0 in taxes.");
-	}
-}
-
-alert("ABC 321");
-
-var john = new Person("John Doe", "blue");
-john.greet();
-
-var jane = new Adult("Jane Smith", "orange");
-jane.greet()
-jane.payTaxes();
-
-$("h1").remove();
+var mobileMenu = new MobileMenu();
